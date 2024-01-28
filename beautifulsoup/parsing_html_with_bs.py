679505fd12a450cb38eef15e7dadb8150dcda278
saulo-miranda <saulo.miranda@ufv.br>
2024-01-28 19:52:25 -0300

    exemplo BS4 navegação em DOM

--- beautifulsoup/parsing_html_with_bs.py
+++ beautifulsoup/parsing_html_with_bs.py
@@ -4,4 +4,10 @@ from bs4 import BeautifulSoup
 html = requests.get("http://localhost:8080/planets.html").text
 
 soup = BeautifulSoup(html, "lxml")
-print(str(soup)[:1000])
+#print(str(soup)[:1000])
+
+#print(str(soup.html.body.div.table)[:200])
+
+#print([str(c) for c in soup.html.body.div.table.children])
+
+print(str(soup.html.body.div.table.tr.parent)[:200])
